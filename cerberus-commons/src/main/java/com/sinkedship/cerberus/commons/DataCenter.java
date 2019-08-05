c1513bfa46832d8ad5c78bcd374155d4baed8ee4
dxGuan <guanyonghui@sinkedship.com>
2019-08-05 12:25:35 +0800

    add data center

---
+++ cerberus-commons/src/main/java/com/sinkedship/cerberus/commons/DataCenter.java
@@ -0,0 +1,17 @@
+package com.sinkedship.cerberus.commons;
+
+/**
+ * Enumeration of Data Center.
+ *
+ * @author Derrick Guan
+ */
+public enum DataCenter {
+
+    ZOOKEEPER,
+
+    CONSUL,
+
+    ETCD,
+
+    LOCAL,
+}
