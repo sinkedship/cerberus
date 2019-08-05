8035800854b337b35b881af5674cfbe8938b0562
dxGuan <guanyonghui@sinkedship.com>
2019-08-05 14:37:39 +0800

    add abstract cerberus config

---
+++ cerberus-commons/src/main/java/com/sinkedship/cerberus/commons/config/AbstractCerberusConfig.java
@@ -0,0 +1,44 @@
+package com.sinkedship.cerberus.commons.config;
+
+import com.sinkedship.cerberus.commons.DataCenter;
+import com.sinkedship.cerberus.commons.config.data_center.*;
+import com.sinkedship.cerberus.commons.exception.CerberusException;
+
+/**
+ * @author Derrick Guan
+ */
+public abstract class AbstractCerberusConfig {
+
+    private final DataCenterConfig dataCenterConfig;
+
+    public AbstractCerberusConfig(DataCenter dataCenter) {
+        switch (dataCenter) {
+            case ZOOKEEPER:
+                dataCenterConfig = new ZookeeperConfig();
+                return;
+            case CONSUL:
+                dataCenterConfig = new ConsulConfig();
+                return;
+            case ETCD:
+                dataCenterConfig = new EtcdConfig();
+                return;
+            case LOCAL:
+                dataCenterConfig = new LocalConfig();
+                return;
+            default:
+                throw new CerberusException("Invalid data center");
+        }
+    }
+
+    public <C extends DataCenterConfig> C getConcreteDataCenterConfig(Class<C> clz) {
+        try {
+            return clz.cast(dataCenterConfig);
+        } catch (ClassCastException e) {
+            throw new CerberusException(e);
+        }
+    }
+
+    public DataCenterConfig getDataCenterConfig() {
+        return dataCenterConfig;
+    }
+}
