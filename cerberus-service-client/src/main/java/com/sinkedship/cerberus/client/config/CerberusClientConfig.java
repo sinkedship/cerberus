f75da50b67e8c35bd041d9d51e9696c2f3a3370d
dxGuan <guanyonghui@sinkedship.com>
2021-07-02 10:55:20 +0800

    add client netty config for cerberus client config

--- cerberus-service-client/src/main/java/com/sinkedship/cerberus/client/config/CerberusClientConfig.java
+++ cerberus-service-client/src/main/java/com/sinkedship/cerberus/client/config/CerberusClientConfig.java
@@ -4,32 +4,45 @@ import io.airlift.drift.transport.netty.client.DriftNettyClientConfig;
 import com.sinkedship.cerberus.commons.config.AbstractCerberusConfig;
 import com.sinkedship.cerberus.commons.DataCenter;
 import com.sinkedship.cerberus.commons.config.data_center.DataCenterConfig;
+import io.airlift.drift.transport.netty.client.DriftNettyConnectionFactoryConfig;
 
 /**
  * Client configuration used to discover dedicated Thrift services.
  * <p>
- * This class contains two independent configuration:
+ * This class contains three independent configuration:
  * <p>
  * a concrete implementation of {@link DataCenterConfig} and
  * a drift netty client config of {@link io.airlift.drift.transport.netty.client.DriftNettyClientConfig}
+ * a drift netty connection factory config of {@link io.airlift.drift.transport.netty.client.DriftNettyConnectionFactoryConfig}
  *
  * @author Derrick Guan
  */
 public final class CerberusClientConfig extends AbstractCerberusConfig {
 
-    private DriftNettyClientConfig driftNettyClientConfig;
+    private final DriftNettyClientConfig driftNettyClientConfig;
+
+    private final DriftNettyConnectionFactoryConfig connectionFactoryConfig;
 
     public CerberusClientConfig(DataCenter dataCenter) {
         super(dataCenter);
         driftNettyClientConfig = new DriftNettyClientConfig();
+        connectionFactoryConfig = new DriftNettyConnectionFactoryConfig();
     }
 
-    public CerberusClientConfig(DataCenter dataCenter, DriftNettyClientConfig driftNettyClientConfig) {
+    public CerberusClientConfig(DataCenter dataCenter,
+                                DriftNettyClientConfig driftNettyClientConfig,
+                                DriftNettyConnectionFactoryConfig connectionFactoryConfig
+                                ) {
         super(dataCenter);
         this.driftNettyClientConfig = driftNettyClientConfig;
+        this.connectionFactoryConfig = connectionFactoryConfig;
     }
 
     public DriftNettyClientConfig getDriftNettyClientConfig() {
         return driftNettyClientConfig;
+    }
+
+    public DriftNettyConnectionFactoryConfig getDriftNettyConnFactoryConfig() {
+        return connectionFactoryConfig;
     }
 }
